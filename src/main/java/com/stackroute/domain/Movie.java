d752b1d2355e9257d80029f786cb71f8148db7f2
satchida <you@example.com>
2019-07-12 10:06:47 +0530

    First commit aware-interface

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,6 +1,13 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
 
 
     private  Actor actor1,actor2,actor3;
@@ -35,5 +42,20 @@ public class Movie {
                 ", actor2=" + actor2 +
                 ", actor3=" + actor3 +
                 '}';
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println(beanFactory);
+
+    }
+
+    public void setBeanName(String s) {
+        System.out.println(s);
+
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println(applicationContext);
+
     }
 }
